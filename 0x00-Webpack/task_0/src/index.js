ea1ecf0dc7e274db46f86d42289a53f7a9c8a59f
Haywhiz <babarindeademola@gmail.com>
2023-02-23 18:24:11 +0100

    webpack installation

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+// Import jQuery
+import $ from 'jquery';
+
+// Add three paragraphs to the page body
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
